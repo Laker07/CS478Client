5b9c7f41d739137f176ab3826859e31d90cd4482
tony <anthony@amartinez.cc>
2017-03-30 22:00:13 -0700

    Updated db and loginActivity.  OnClick of friend thier name is saved to preferences.

--- Crypto/app/src/main/java/com/example/tony/crypto/Chat/LoginActivity.java
+++ Crypto/app/src/main/java/com/example/tony/crypto/Chat/LoginActivity.java
@@ -16,7 +16,6 @@ package com.example.tony.crypto.Chat;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.nfc.Tag;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -24,7 +23,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -34,11 +32,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.tony.crypto.DB.DBHandler;
-import com.example.tony.crypto.EncDec.Encrypt;
-import com.example.tony.crypto.EncDec.Keys;
 import com.example.tony.crypto.EncDec.RSAKeyGen;
 import com.example.tony.crypto.POJOS.Friend;
 import com.example.tony.crypto.POJOS.Register;
@@ -47,13 +42,9 @@ import com.example.tony.crypto.R;
 import com.google.gson.Gson;
 
 import org.json.JSONObject;
-
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.KeyPair;
-import java.security.spec.InvalidKeySpecException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
@@ -81,66 +72,57 @@ public class LoginActivity extends AppCompatActivity {
 
         name = (EditText) findViewById(R.id.username);
         pwds = (EditText) findViewById(R.id.password);
-        Button submit = (Button)findViewById(R.id.buttonSubmit);
+        Button login = (Button)findViewById(R.id.buttonSubmit);
         Button register = (Button)findViewById(R.id.register);
         requestQueue = Volley.newRequestQueue(getApplicationContext());
 
-        //db
-
-        String barr = "hello im' private";
-        String carr = "hello i'm public";
-
         final DBHandler db = new DBHandler(this);
-//        Log.d("Insert: ", "Inserting....");
-//        db.addMe(new Friend("one"));
-//        db.addMe(new Friend("two"));
+//        Log.d("Reading entries","reading......");
+//        List<Friend> friends = db.getAllFriends();
+//        for(Friend f: friends){
+//            String log = "Name : " + f.getName() +" public key : " + f.getPublicKey();
+//            Log.d("Name: ", log );
+//        }
 
-        Log.d("Reading entries","reading......");
-        List<Friend> friends = db.getAllFriends();
-
-        for(Friend f: friends){
-            String log = "Name : " + f.getName();
-
-
-            Log.d("Name: ", log );
-        }
-
-        submit.setOnClickListener(new View.OnClickListener(){
+        login.setOnClickListener(new View.OnClickListener(){
             Context c = getApplicationContext();
-
             @Override
             public void onClick(View v){
+                //volley json post request
                 JsonObjectRequest reqPost = new JsonObjectRequest(Request.Method.POST, ENDPOINTSIGNIN, null,
-                        new Response.Listener<JSONObject>() {
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            /*receive json obj and pass it to a Register obj with the properties:
+                                 String response;
+                                 String message;
+                                 String jwtToken;
+                             */
+                            Gson gson = new Gson();
+                            reg = gson.fromJson(response.toString(), Register.class);
+                            //if login error then toast message to screen
+                            if(reg.getResponse().equals("Error")){
+                                Log.d("if error: ." ,reg.getMessage());
+                                CharSequence text = reg.getMessage();
+                                int duration = Toast.LENGTH_SHORT;
+                                Toast toast = Toast.makeText(c, text,duration);
+                                toast.show();
 
-                            @Override
-                            public void onResponse(JSONObject response) {
-                                Gson gson = new Gson();
-                                reg = gson.fromJson(response.toString(), Register.class);
-
-                                if(reg.getResponse().equals("Error")){
-                                    Log.d("if error: ." ,reg.getMessage());
-                                    CharSequence text = reg.getMessage();
-                                    int duration = Toast.LENGTH_SHORT;
-                                    Toast toast = Toast.makeText(c, text,duration);
-                                    toast.show();
-
-                                }else{
-                                    //log and verify
-                                    Log.d("all", response.toString());
-                                    Log.d("response : ", reg.getResponse());
-                                    Log.d("msg      : ", reg.getMessage());
-                                    Log.d("jwt      : ", reg.getJwt());
-                                    //place jwt and username in preferences
-                                    editor.putString("jwt", reg.getJwt());
-                                    editor.putString("name", name.getText().toString());
-                                    editor.commit();
-                                    //switch screens
-                                    Intent intent = new Intent(getApplicationContext(), MessageList.class);
-                                    startActivity(intent);
-                                }
+                            }else{
+                                //log and verify
+                                Log.d("response : ", reg.getResponse());
+                                Log.d("msg      : ", reg.getMessage());
+                                Log.d("jwt      : ", reg.getJwt());
+                                //place jwt and username in preferences
+                                editor.putString("jwt", reg.getJwt());
+                                editor.putString("name", name.getText().toString());
+                                editor.commit();
+                                //switch screens
+                                Intent intent = new Intent(getApplicationContext(), MessageList.class);
+                                startActivity(intent);
                             }
-                        }, new Response.ErrorListener() {
+                        }
+                    }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         //network error response
@@ -163,21 +145,27 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-
+        //register new user
         register.setOnClickListener(new View.OnClickListener(){
             Context c = getApplicationContext();
-
             @Override
             public void onClick(View v){
                 JsonObjectRequest reqPost = new JsonObjectRequest(Request.Method.POST, ENDPOINTREGISTER, null,
-                        new Response.Listener<JSONObject>() {
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            //create new object to hold json response
+                            Gson gson = new Gson();
+                            reg = gson.fromJson(response.toString(), Register.class);
 
-                            @Override
-                            public void onResponse(JSONObject response) {
-                                //create new object to hold json response
-                                Gson gson = new Gson();
-                                reg = gson.fromJson(response.toString(), Register.class);
+                            if(reg.getResponse().equals("Error")){
+                                Log.d("if error: ." ,reg.getMessage());
+                                CharSequence text = reg.getMessage();
+                                int duration = Toast.LENGTH_SHORT;
+                                Toast toast = Toast.makeText(c, text,duration);
+                                toast.show();
 
+                            }else {
                                 //log and verify
                                 Log.d("all", response.toString());
                                 Log.d("response : ", reg.getResponse());
@@ -194,55 +182,34 @@ public class LoginActivity extends AppCompatActivity {
                                 //this needs to mode to another thread
                                 //waits will key is generated and written to db
                                 KeyPair key = RSAKeyGen.generate();
+
+                                //possible unique constraint exception
                                 db.addMe(new Friend(name.getText().toString(), RSAKeyGen.getPublic(key), RSAKeyGen.getPrivate(key)));
-
-                                Log.d("Reading entries","reading......");
-                                Friend f = null;
-                               // List<Friend> friends = null;
-                                int count =0;
-                                try {
-                                    f = db.getFriend(name.getText().toString());
-                                 //   friends = db.getAllFriends();
-                                } catch (InvalidKeySpecException e) {
-                                    e.printStackTrace();
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                }
-//                                for(Friend fn: friends) {
-//                                    String log = "Name : " + f.getName();
-//                                    Log.d("Name: ", log);
-//                                }
-
-
-                                //switch screens
-                                Intent intent = new Intent(getApplicationContext(), Messenger.class);
+                                Intent intent = new Intent(getApplicationContext(), MessageList.class);
                                 startActivity(intent);
-
                             }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
 
-                        VolleyLog.e("Volley Error: ", error.getMessage());
-                    }
-                }){
-                    //set header content type to application json
-                    @Override
-                    public Map<String, String> getHeaders() throws AuthFailureError {
-                        return header();
-                    }
-                    //send json with (username, password) using Gson
-                    @Override
-                    public byte[] getBody() {
-                       return body();
-                    }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    VolleyLog.e("Volley Error: ", error.getMessage());
+                }
+            }){
+                //set header content type to application json
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    return header();
+                }
+                //send json with (username, password) using Gson
+                @Override
+                public byte[] getBody() {
+                   return body();
+                }
                 };
                 requestQueue.add(reqPost);
             }
         } );
-
-
-
 
 
     }//end onCreate
--- Crypto/app/src/main/java/com/example/tony/crypto/DB/DBHandler.java
+++ Crypto/app/src/main/java/com/example/tony/crypto/DB/DBHandler.java
@@ -9,43 +9,19 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
 import com.example.tony.crypto.POJOS.Friend;
-
-import org.spongycastle.jce.provider.BouncyCastleProvider;
-import org.spongycastle.openssl.PEMParser;
-import org.spongycastle.util.io.pem.PemReader;
-
-import java.io.IOException;
-import java.io.StringReader;
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.Security;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.X509EncodedKeySpec;
 import java.util.ArrayList;
-import java.util.List;
-
-import javax.crypto.Cipher;
 
 public class DBHandler extends SQLiteOpenHelper{
-    //Will use BouncyCastle as the provider
-    static {
-        Security.addProvider(new BouncyCastleProvider());
-    }
 
-
-    // All Static variables
     // Database Version
     private static final int DATABASE_VERSION = 1;
 
     // Database Name
-    private static final String DATABASE_NAME = "friendManager";
+    private static final String DATABASE_NAME = "friendManager1";
 
     // Contacts table name
-    private static final String TABLE_KEYS = "keyHolder";
+    private static final String TABLE_KEYS = "keyHolder1";
 
     // Contacts Table Columns names
     private static final String KEY_NAME = "name";
@@ -56,10 +32,15 @@ public class DBHandler extends SQLiteOpenHelper{
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    //deletes the table, but not the db
+    public void dropT(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DROP TABLE " + TABLE_KEYS);
+    }
+
     // Creating Tables
     @Override
     public void onCreate(SQLiteDatabase db) {
-
         //db.execSQL("DROP TABLE IF EXIST " + TABLE_KEYS);
         String CREATE_KEYS_TABLE = "CREATE TABLE " + TABLE_KEYS + "("
                 + KEY_NAME + " TEXT PRIMARY KEY,"
@@ -67,7 +48,6 @@ public class DBHandler extends SQLiteOpenHelper{
                 + KEY_PR_KEY + " TEXT" + ")";
         db.execSQL(CREATE_KEYS_TABLE);
     }
-
 
     // Upgrading database
     @Override
@@ -78,10 +58,6 @@ public class DBHandler extends SQLiteOpenHelper{
         // Create tables again
         onCreate(db);
     }
-
-    /**
-     * All CRUD(Create, Read, Update, Delete) Operations
-     */
 
     // Adding new friend, only private key
     public void addFriend(Friend friend) {
@@ -103,8 +79,8 @@ public class DBHandler extends SQLiteOpenHelper{
 
         ContentValues values = new ContentValues();
         values.put(KEY_NAME, friend.getName()); // friend Name
-        values.put(KEY_PU_KEY, friend.getPublicKey()); // friend key
-        values.put(KEY_PR_KEY, friend.getPrivateKey()); // friend key
+        values.put(KEY_PU_KEY, friend.getPublicKey()); // public key
+        values.put(KEY_PR_KEY, friend.getPrivateKey()); // private key
 
         // Inserting Row
         db.insert(TABLE_KEYS, null, values);
@@ -112,7 +88,7 @@ public class DBHandler extends SQLiteOpenHelper{
     }
 
     // Getting single friend
-    public Friend getFriend(String name) throws InvalidKeySpecException, IOException {
+    public Friend getFriend(String name){
         SQLiteDatabase db = this.getReadableDatabase();
 
         Cursor cursor = db.query(TABLE_KEYS, new String[] { KEY_NAME,
@@ -120,7 +96,6 @@ public class DBHandler extends SQLiteOpenHelper{
                 new String[] { name }, null, null, null, null);
         if (cursor != null)
             cursor.moveToFirst();
-
 
         Friend friend = new Friend(cursor.getString(0),cursor.getString(1), cursor.getString(2));
         // return contact
@@ -139,14 +114,10 @@ public class DBHandler extends SQLiteOpenHelper{
         // looping through all rows and adding to list
         if (cursor.moveToFirst()) {
             do {
-
-
-                Friend friend = new Friend(cursor.getString(0));
-
+                Friend friend = new Friend(cursor.getString(0),cursor.getString(1), cursor.getString(2));
                 friendList.add(friend);
             } while (cursor.moveToNext());
         }
-
         // return contact list
         return friendList;
     }
@@ -170,7 +141,6 @@ public class DBHandler extends SQLiteOpenHelper{
                 new String[] { String.valueOf(friend.getName()) });
         db.close();
     }
-
 
     // Getting friends count
     public int getFriendCount() {
